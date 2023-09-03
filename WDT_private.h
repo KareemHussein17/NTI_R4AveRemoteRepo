d17d21806edceb50efe018bbf96f592a77ef8ec9
Your Name <kareemmhusamit@gmail.com>
2023-09-03 15:52:35 +0300

    v2

--- WDT_private.h
+++ WDT_private.h
@@ -13,11 +13,7 @@
 								 
 // Watchdog Timer Control Register
 #define WDTCR             (*(volatile u8*)0x41)
-#define WDP0              0
-#define WDP1              1
-#define WDP2              2
-#define WDE               3
-#define WDTOE             4
+
 
 
 #endif /* WDT_PRIVATE_H_ */
